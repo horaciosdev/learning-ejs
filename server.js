91b8ddc08a9f65bc93c0521de1c429b9368489e2
Horacio <horacio.schumann@gmail.com>
2023-02-08 22:48:15 -0300

    View files organized in separate directories ('pages' and 'partials').

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("pages/index");
 });
 
 app.get("/sobre", function (req, res) {
-  res.render("about");
+  res.render("pages/about");
 });
 
 const PORT = process.env.PORT || 8080;
